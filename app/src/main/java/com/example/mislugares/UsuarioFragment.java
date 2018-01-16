0701ebd6aedf60b4a38f1ce33b5aad1f6f4bd777
Jorge Vilches <jvg637@gmail.com>
2018-01-16 23:48:54 +0100

    versioin inicial

---
+++ app/src/main/java/com/example/mislugares/UsuarioFragment.java
@@ -0,0 +1,85 @@
+package com.example.mislugares;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.LruCache;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+import com.android.volley.toolbox.Volley;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * Created by jvg63 on 13/01/2018.
+ */
+
+public class UsuarioFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflador, ViewGroup contenedor, Bundle savedInstanceState) {
+        View vista = inflador.inflate(R.layout.fragment_usuario, contenedor, false);
+        FirebaseUser usuario = FirebaseAuth.getInstance().getCurrentUser();
+        TextView nombre = (TextView) vista.findViewById(R.id.nombre);
+        nombre.setText(usuario.getDisplayName());
+
+        TextView correo = (TextView) vista.findViewById(R.id.correo);
+        correo.setText(usuario.getDisplayName());
+
+        TextView proveedor = (TextView) vista.findViewById(R.id.proveedor);
+        proveedor.setText(usuario.getProviders().toString());
+
+        TextView telefono = (TextView) vista.findViewById(R.id.telefono);
+        telefono.setText(usuario.getPhoneNumber());
+
+
+        Button cerrarSesion = (Button) vista.findViewById(R.id.btn_cerrar_sesion);
+        cerrarSesion.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                AuthUI.getInstance().signOut(getActivity()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent i = new Intent(getActivity(), CustomLoginActivity.class);
+                        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(i);
+                        getActivity().finish();
+                    }
+                });
+            }
+        });
+
+        // Inicialización Volley  (Hacer solo una vez en Singleton o Applicaction)
+        RequestQueue colaPeticiones = Volley.newRequestQueue(getActivity());
+        ImageLoader lectorImagenes = new ImageLoader(colaPeticiones, new ImageLoader.ImageCache() {
+            private final LruCache<String, Bitmap> cache = new LruCache<String, Bitmap>(10);
+
+            public void putBitmap(String url, Bitmap bitmap) {
+                cache.put(url, bitmap);
+            }
+
+            public Bitmap getBitmap(String url) {
+                return cache.get(url);
+            }
+        });
+
+        // Foto de usuario
+        Uri urlImagen = usuario.getPhotoUrl();
+        if (urlImagen != null) {
+            NetworkImageView fotoUsuario = (NetworkImageView) vista.findViewById(R.id.imagen);
+            fotoUsuario.setImageUrl(urlImagen.toString(), lectorImagenes);
+        }
+        return vista;
+    }
+}
